def8bbc70a3143763dcda2656825e54ab14177da
scheik <scheik.todeswache@googlemail.com>
2014-11-06 18:32:18 +0100

    Update code, cause AVR-Master is no more receiving RX data interrupt driven Robot_RPi_setSpeed_main.cpp: Update code

--- src/Robot_RPi_setSpeed_main.cpp
+++ src/Robot_RPi_setSpeed_main.cpp
@@ -34,11 +34,11 @@ int main(int argc, char *argv[]){
 	// to code
 	filedesc = openSerialPort("/dev/ttyAMA0", B38400);					// open RPis serial port
 	if (filedesc == -1) exit(1);
-	usleep(80000);														// Sleep for UART to power up and set options
+	usleep(40000);														// Sleep for UART to power up and set options
 	set_Speed1=strtol(argv[1], NULL, 10);								//convert string of argument1 to decimal
 	set_Speed2=strtol(argv[2], NULL, 10);
 	set_MD49_Speed();
-	//close(fd);															// Close port
+	close(fd);															// Close port
 	return 0;															// exit
 }//end.mainfunction
 
@@ -47,25 +47,7 @@ void set_MD49_Speed (void){
 	serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
 	serialBuffer[2] = set_Speed1;					// speed1
 	serialBuffer[3] = set_Speed2;					// speed2
-	serialBuffer[4] = 0;							// all following bytes are
-	serialBuffer[5] = 0;							// dummy-bytes to
-	serialBuffer[6] = 0;							// fill message
-	serialBuffer[7] = 0;
-	serialBuffer[8] = 0;
-	serialBuffer[9] = 0;
-	serialBuffer[10] = 0;
-	serialBuffer[11] = 0;
-	serialBuffer[12] = 0;
-	serialBuffer[13] = 0;
-	serialBuffer[14] = 0;
-	serialBuffer[15] = 0;
-	writeBytes(fd, 16);								// write 16 Bytes from serialbuffer to UART
-	usleep(50000);
-	writeBytes(fd, 16);
-	usleep(50000);
-	writeBytes(fd, 16);
-
-
+	writeBytes(fd, 4);
 }
 
 
